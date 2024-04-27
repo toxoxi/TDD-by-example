4b524b329a92d6aeff655792dd1498c0a676e5a3
toxoxi <yorori.too@gmail.com>
2024-04-27 13:47:20 +0900

    Move Dollar and Franc classes into money.ts to avoid the circular dependency

--- multi_currency/money.ts
+++ multi_currency/money.ts
@@ -1,6 +1,3 @@
-import Dollar from "./dollar";
-import Franc from "./franc";
-
 export default abstract class Money {
   protected amount: number;
 
@@ -20,5 +17,35 @@ export default abstract class Money {
 
   static franc(amount: number): Money {
     return new Franc(amount);
+  }
+}
+
+class Dollar extends Money {
+  constructor(amount: number) {
+    super();
+    this.amount = amount;
+  }
+
+  currency(): string {
+    return "USD";
+  }
+
+  times(multiplier: number): Money {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+class Franc extends Money {
+  constructor(amount: number) {
+    super();
+    this.amount = amount;
+  }
+
+  currency(): string {
+    return "CHF";
+  }
+
+  times(multiplier: number): Money {
+    return new Franc(this.amount * multiplier);
   }
 }
