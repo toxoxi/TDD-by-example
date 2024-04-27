92160dbe7e3774a985ba3b36e31f83e703828624
toxoxi <yorori.too@gmail.com>
2024-04-27 14:02:17 +0900

    Remove Dollar and Franc classes

--- multi_currency/money.spec.ts
+++ multi_currency/money.spec.ts
@@ -12,7 +12,7 @@
  * [x] 5CHF * 2 = 10CHF
  * [ ] Dollar と Franc の重複
  * [x] equals の一般化
- * [ ] times の一般化
+ * [x] times の一般化
  * [x] Franc と Dollar を比較する
  * [x] 通貨の概念
  * [ ] testFrancMultiplication を削除する？
@@ -27,35 +27,15 @@ describe("Money", () => {
     expect(Money.franc(1).currency()).toBe("CHF");
   });
 
-  describe("Dollar class", () => {
-    test("multiplication", () => {
-      const five = Money.dollar(5);
-      expect(five.times(2).equals(Money.dollar(10))).toBe(true);
-      expect(five.times(3).equals(Money.dollar(15))).toBe(true);
-    });
-
-    test("equality", () => {
-      expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
-      expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
-    });
+  test("equality", () => {
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+    expect(Money.dollar(10).equals(Money.franc(10))).toBe(false);
   });
 
-  describe("Franc class", () => {
-    test("multiplication", () => {
-      const five = Money.franc(5);
-      expect(five.times(2).equals(Money.franc(10))).toBe(true);
-      expect(five.times(3).equals(Money.franc(15))).toBe(true);
-    });
-
-    test("equality", () => {
-      expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
-      expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
-    });
-  });
-
-  describe("Cross-currency", () => {
-    test("equality", () => {
-      expect(Money.dollar(10).equals(Money.franc(10))).toBe(false);
-    });
+  test("multiplication", () => {
+    const five = Money.dollar(5);
+    expect(five.times(2).equals(Money.dollar(10))).toBe(true);
+    expect(five.times(3).equals(Money.dollar(15))).toBe(true);
   });
 });
--- multi_currency/money.ts
+++ multi_currency/money.ts
@@ -1,51 +1,31 @@
-export default abstract class Money {
+type Currency = "USD" | "CHF";
+
+export default class Money {
   protected amount: number;
+  protected _currency: Currency;
 
-  abstract currency(): string;
-  abstract times(multiplier: number): Money;
+  constructor(amount: number, currency: Currency) {
+    this.amount = amount;
+    this._currency = currency;
+  }
 
-  equals(money: Money) {
-    return (
-      this.amount === money.amount &&
-      this.constructor.name === money.constructor.name
-    );
+  currency() {
+    return this._currency;
+  }
+
+  times(multiplier: number): Money {
+    return new Money(this.amount * multiplier, this._currency);
+  }
+
+  equals(money: Money): boolean {
+    return this.amount === money.amount && this._currency === money.currency();
   }
 
   static dollar(amount: number): Money {
-    return new Dollar(amount);
+    return new Money(amount, "USD");
   }
 
   static franc(amount: number): Money {
-    return new Franc(amount);
-  }
-}
-
-class Dollar extends Money {
-  constructor(amount: number) {
-    super();
-    this.amount = amount;
-  }
-
-  currency(): string {
-    return "USD";
-  }
-
-  times(multiplier: number): Money {
-    return new Dollar(this.amount * multiplier);
-  }
-}
-
-class Franc extends Money {
-  constructor(amount: number) {
-    super();
-    this.amount = amount;
-  }
-
-  currency(): string {
-    return "CHF";
-  }
-
-  times(multiplier: number): Money {
-    return new Franc(this.amount * multiplier);
+    return new Money(amount, "CHF");
   }
 }
